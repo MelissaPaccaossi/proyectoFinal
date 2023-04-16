7cdd4c4024bafa1c28580fc9152dcee9d42ff0c4
Patricio <patricioprp06@gmail.com>
2023-04-16 10:55:31 -0300

    se instala libreria React Native Elements

--- App.js
+++ App.js
@@ -2,6 +2,18 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import BuscarGrupo from './src/screens/BuscarGrupo';
 
+// <style type="text/css">{`
+//   @font-face {
+//     font-family: 'MaterialIcons';
+//     src: url(${require('react-native-vector-icons/Fonts/MaterialIcons.ttf')}) format('truetype');
+//   }
+
+//   @font-face {
+//     font-family: 'FontAwesome';
+//     src: url(${require('react-native-vector-icons/Fonts/FontAwesome.ttf')}) format('truetype');
+//   }
+// `}</style>
+
 
 const navigator = createStackNavigator({
   BuscarG: BuscarGrupo
